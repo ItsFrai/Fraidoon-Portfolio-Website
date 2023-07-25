93d44bb1b2d73cf02e2aff0c28730b04bc67c1af
ItsFrai <fpourooshasb@gmail.com>
2023-07-25 10:47:12 -0400

    part of computer three.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
  import React from 'react'
 
+ import {styles} from '../styles'
+ import {ComputersCanvas} from './canvas'
+
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-6 h-6 rounded-full bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'></div>
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hello, my name is <span className='text-[#915eff]'>Fraidoon</span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                I am a student at Rutgers who  <br className='sm:block hidden' />loves to code and develop apps/websites!
+            </p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+    </section>
   )
 }
 
